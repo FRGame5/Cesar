278091bd5f6e17e3b6ff6055f662ef0a3568b284
FRGame5 <paillex.loic@gmail.com>
2015-09-27 22:23:29 +0200

    Changement du nom des variable

--- Cesar/src/Cesar.java
+++ Cesar/src/Cesar.java
@@ -1,71 +1,59 @@
 import java.util.Arrays;
 
-
 public class Cesar {
-	private int msg[];
-	private int msgCode[];
+	private int entree[];
+	private int sortie[];
 	private int freq[];
 	private Character cle;
 	private Alphabet a;
-	
-	
-	public Cesar(String msg, char cle)
-	{
+
+	public Cesar(String entree, char cle) {
 		a = new Alphabet();
-		this.msg = new int[msg.length()];
-		this.msgCode = new int[msg.length()];
+		this.entree = new int[entree.length()];
+		this.sortie = new int[entree.length()];
 		this.cle = cle;
-		msg = msg.toUpperCase();
-		for(int i = 0;i<msg.length();i++)
-		{
-			this.msg[i] = a.position(msg.charAt(i));
+		entree = entree.toUpperCase();
+		for (int i = 0; i < entree.length(); i++) {
+			this.entree[i] = a.position(entree.charAt(i));
 		}
-		
+
 	}
-	
-	public void coder()
-	{
-		for(int i=0; i<msg.length;i++)
-		{
-			msgCode[i] = (msg[i]+a.position(cle))%27;
+
+	public void coder() {
+		for (int i = 0; i < entree.length; i++) {
+			sortie[i] = (entree[i] + a.position(cle)) % 27;
 		}
 	}
-	
-	public String getMsg()
-	{
+
+	public String getEntree() {
 		String retour = new String("");
-		for (int i =0;i<msg.length;i++) {
-			retour = retour.concat(a.lettreCorrespondante(msg[i]).toString());
+		for (int i = 0; i < entree.length; i++) {
+			retour = retour.concat(a.lettreCorrespondante(entree[i]).toString());
 		}
 		return retour;
 	}
-	
-	public String getMsgCode()
-	{
+
+	public String getsortie() {
 		String retour = new String("");
-		for (int i =0;i<msgCode.length;i++) {
-			retour = retour.concat(a.lettreCorrespondante(msgCode[i]).toString());
+		for (int i = 0; i < sortie.length; i++) {
+			retour = retour.concat(a.lettreCorrespondante(sortie[i]).toString());
 		}
 		return retour;
 	}
-	
-	public int[] getFrequenceLettre()
-	{
+
+	public int[] getFrequenceLettre() {
 		int retour[] = new int[27];
-		for(int i=0;i<msg.length;i++)
-		{
-			retour[msg[i]]+=1;
+		for (int i = 0; i < entree.length; i++) {
+			retour[entree[i]] += 1;
 		}
 		this.freq = retour;
 		return retour;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Cesar [msg=" + Arrays.toString(msg) + ", msgCode=" + Arrays.toString(msgCode) + ", freq="
+		return "Cesar [entree=" + Arrays.toString(entree) + ", sortie=" + Arrays.toString(sortie) + ", freq="
 				+ Arrays.toString(freq) + ", cle=" + cle + "]";
 	}
-	
-	
 
 }
