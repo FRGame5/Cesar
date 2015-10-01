8bbbe7a4cb51e371fdbcf306779283b69635d7d4
FRGame5 <paillex.loic@gmail.com>
2015-10-01 22:34:55 +0200

    Modification et amelioration de tout le code Cesar

--- Cesar/src/Cesar.java
+++ Cesar/src/Cesar.java
@@ -1,88 +1,53 @@
 import java.util.Arrays;
 
 public class Cesar {
-	private int entree[];
-	private int sortie[];
-	private int freq[];
-	private Character cle;
 	private Alphabet a;
 
-	public Cesar(String entree, char cle) {
+	public Cesar() {
 		a = new Alphabet();
-		this.entree = new int[entree.length()];
-		this.sortie = new int[entree.length()];
-		this.freq = new int[27];
-		this.cle = cle;
-		entree = entree.toUpperCase();
-		for (int i = 0; i < entree.length(); i++) {
-			this.entree[i] = a.position(entree.charAt(i));
-		}
-
 	}
 
-	public void coder() {
-		for (int i = 0; i < entree.length; i++) {
-			sortie[i] = (entree[i] + a.position(cle)) % 27;
-			if (sortie[i] == 0)
-				sortie[i]++;
-		}
-	}
-
-	public void deCoder() {
-		for (int i = 0; i < entree.length; i++) {
-			sortie[i] = (entree[i] - a.position(cle));
-			if (sortie[i] < 1)
-				sortie[i] += 26;
-			if (entree[i] == 0)
-				sortie[i] = 0;
-		}
-	}
-
-	public String getEntree() {
-		String retour = new String("");
-		for (int i = 0; i < entree.length; i++) {
-			retour = retour.concat(a.lettreCorrespondante(entree[i]).toString());
-		}
-		return retour;
-	}
-
-	public String getSortie() {
-		String retour = new String("");
-		for (int i = 0; i < sortie.length; i++) {
-			retour = retour.concat(a.lettreCorrespondante(sortie[i]).toString());
-		}
-		return retour;
-	}
-
-	public int[] getFrequenceLettre() {
-		int retour[] = new int[27];
-		for (int i = 0; i < entree.length; i++) {
-			retour[entree[i]] += 1;
-		}
-		this.freq = retour;
-		return retour;
-	}
-
-	public char trouverClee() {
-		int max = 0;
-		int lettre = 0;
-		for(int i = 1; i<freq.length;i++)
+	public char decaler(char entree, int decalage)
+	{
+		if(a.position(entree) == 0)
+			return ' ';
+		if(decalage<=0)
 		{
-			if(max<freq[i])
-			{
-				lettre = i;
-				max = freq[i];
-			}
-			
+			if(a.lettreCorrespondante(-decalage) == entree)
+				return 'Z';
+			return a.lettreCorrespondante((a.position(entree)+ 26 + decalage)%26);
+		}else
+		{
+			return a.lettreCorrespondante((a.position(entree) + decalage)%26);
 		}
-		return a.lettreCorrespondante(lettre-5); // lettre E
 	}
 
-	@Override
-	public String toString() {
-		return "Cesar [entree=" + Arrays.toString(entree) + ", sortie="
-				+ Arrays.toString(sortie) + ", freq=" + Arrays.toString(freq)
-				+ ", cle=" + cle + "]";
+	public String coder(String entree, char cle) {
+		String sortie = new String();
+		for (int i = 0; i<entree.length();i++)
+		{
+			sortie = sortie.concat(Character.toString(this.decaler(entree.charAt(i),a.position(cle))));
+		}
+		return sortie;
+	}
+
+	public String deCoder(String entree, char cle) {
+		String sortie = new String();
+		for (int i = 0; i<entree.length();i++)
+		{
+			sortie = sortie.concat(Character.toString(this.decaler(entree.charAt(i), -a.position(cle))));
+		}
+		return sortie;
+	}
+
+	public int[] frequence(String entree)
+	{
+		int[] freq = new int[27];
+		for(int i = 0; i<entree.length();i++)
+		{
+			freq[a.position(entree.charAt(i))]++;
+		}
+		return freq;
 	}
 
 }
--- Cesar/src/Main.java
+++ Cesar/src/Main.java
@@ -2,10 +2,10 @@
 public class Main {
 
 	public static void main(String[] args) {
+		Cesar c = new Cesar();
 		Alphabet a = new Alphabet();
-		Vigenere v = new Vigenere("si c est jeudi c est qu il y a des frites", "RAVIOLI");
-		v.coder();
-		System.out.println(v.getSortie());
+		System.out.println("Code Cesar");
+
 	}
 
 }
