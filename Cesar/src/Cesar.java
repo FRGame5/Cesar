cebc1149afcaa2d5441eaa51853502bae3afca77
FRGame5 <paillex.loic@gmail.com>
2015-10-01 23:21:36 +0200

    Ajout methode "trouverCle" qui renvoie le charactere pour dechiffrer le message(WIP)

--- Cesar/src/Cesar.java
+++ Cesar/src/Cesar.java
@@ -9,16 +9,16 @@ public class Cesar {
 
 	public char decaler(char entree, int decalage)
 	{
-		if(a.position(entree) == 0)
+		if(this.a.position(entree) == 0)
 			return ' ';
 		if(decalage<=0)
 		{
-			if(a.lettreCorrespondante(-decalage) == entree)
+			if(this.a.lettreCorrespondante(-decalage) == entree)
 				return 'Z';
-			return a.lettreCorrespondante((a.position(entree)+ 26 + decalage)%26);
+			return this.a.lettreCorrespondante((this.a.position(entree)+ 26 + decalage)%26);
 		}else
 		{
-			return a.lettreCorrespondante((a.position(entree) + decalage)%26);
+			return this.a.lettreCorrespondante((this.a.position(entree) + decalage)%26);
 		}
 	}
 
@@ -26,7 +26,7 @@ public class Cesar {
 		String sortie = new String();
 		for (int i = 0; i<entree.length();i++)
 		{
-			sortie = sortie.concat(Character.toString(this.decaler(entree.charAt(i),a.position(cle))));
+			sortie = sortie.concat(Character.toString(this.decaler(entree.charAt(i),this.a.position(cle))));
 		}
 		return sortie;
 	}
@@ -35,7 +35,7 @@ public class Cesar {
 		String sortie = new String();
 		for (int i = 0; i<entree.length();i++)
 		{
-			sortie = sortie.concat(Character.toString(this.decaler(entree.charAt(i), -a.position(cle))));
+			sortie = sortie.concat(Character.toString(this.decaler(entree.charAt(i), - this.a.position(cle))));
 		}
 		return sortie;
 	}
@@ -45,9 +45,35 @@ public class Cesar {
 		int[] freq = new int[27];
 		for(int i = 0; i<entree.length();i++)
 		{
-			freq[a.position(entree.charAt(i))]++;
+			freq[this.a.position(entree.charAt(i))]++;
 		}
 		return freq;
+	}
+
+	public char trouverClee(String entree)
+	{
+		float  freqFr[] = {0F,7.68F,0.8F,3.32F,3.6F,17.76F,1.06F,1.1F,0.64F,7.63F,0.19F,0F,5.89F,2.72F,7.61F,5.34F,3.24F,1.34F,6.81F,8.23F,7.3F,6.05F,1.27F,0F,0.54F,0.21F,0.07F};
+		int freqMot[] = this.frequence(entree);
+		float probat[] = new float[27];
+		int retour = 0;
+		float freqRetour = 0F;
+		for(int i = 0; i<probat.length;i++)
+		{
+			for(int j = 0; j<probat.length;j++)
+			{
+				probat[i] += freqFr[j]*freqMot[(j+i)%27];
+			}
+		}
+		for(int i = 0; i<probat.length;i++)
+		{
+			if(probat[i]>freqRetour)
+			{
+				freqRetour = probat[i];
+				retour = i;
+			}
+		}
+
+		return this.a.lettreCorrespondante(retour+4);
 	}
 
 }
--- Cesar/src/Main.java
+++ Cesar/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 		Cesar c = new Cesar();
 		Alphabet a = new Alphabet();
 		System.out.println("Code Cesar");
-
+		System.out.print(c.deCoder("NQX TSY IJX HMFUJFZC WTSIX ANAJ QF GWJYFLSJ NQX TSY IJX HMFUJFZC WTSIX ANAJ QJX GWJYTSX",c.trouverClee("NQX TSY IJX HMFUJFZC WTSIX ANAJ QF GWJYFLSJ NQX TSY IJX HMFUJFZC WTSIX ANAJ QJX GWJYTSX")));
 	}
 
 }
