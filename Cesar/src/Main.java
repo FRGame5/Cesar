7969e9029d0a1f463a2b5e3bbb3ac9a33abd109b
tatsujb <tatsu@hotmail>
2015-10-02 10:59:09 +0200

    azsfghj

--- Cesar/src/Main.java
+++ Cesar/src/Main.java
@@ -5,7 +5,9 @@ public class Main {
 		Cesar c = new Cesar();
 		Alphabet a = new Alphabet();
 		System.out.println("Code Cesar");
-		System.out.print(c.deCoder("NQX TSY IJX HMFUJFZC WTSIX ANAJ QF GWJYFLSJ NQX TSY IJX HMFUJFZC WTSIX ANAJ QJX GWJYTSX",c.trouverClee("NQX TSY IJX HMFUJFZC WTSIX ANAJ QF GWJYFLSJ NQX TSY IJX HMFUJFZC WTSIX ANAJ QJX GWJYTSX")));
+		//Ceci est un message de test il est tres long j aime le pate et aussi le foie gras des fois je mange des pizza hier je me suis fait preter des tournevis pour manger des pizza
+		//cle F
+		System.out.print());
 	}
 
 }
