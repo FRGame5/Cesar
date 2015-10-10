2c5909ad888f4868c1c2a384603bcc9b30ed90fa
FRGame5 <paillex.loic@gmail.com>
2015-10-10 18:06:44 +0200

    Ajout de la methode decoder sans clé de vigenere

--- Cesar/src/Vigenere.java
+++ Cesar/src/Vigenere.java
@@ -33,6 +33,18 @@ public class Vigenere {
         return sortie;
     }
     
+    public void attaqueBruteForce(String entree)
+    {
+    	for (int i = 0; i < 20; i++) {
+			System.out.println(this.deCoder(entree, this.trouverCle(entree, i+1)));
+		}
+    }
+    
+    /*
+     * Cette fonction renvoie l'indice de coincidence d'un texte
+     * C'est la probabilite que si nous prenons 2 lettre au hasard dans le texte
+     * ce soit les meme
+     */
     public float indiceCoincidence(String entree)
     {
     	entree = entree.toUpperCase();
@@ -47,33 +59,65 @@ public class Vigenere {
     	return Ic;
     }
     
-    public int trouverLongueurCle(String entree)
+    /*
+     * Fonction qui renvoie le message decode sans avoir besoi de la
+     * clée de décodage
+     * Il s'agit d'une methode reprenant la methode cesar
+     * Cette methode ne permet pas en revanche de decoder les message cours
+     */
+    public String decoderSansCle(String entree)
     {
-    	
-    	float n = (float)entree.length() - (float)c.frequence(entree)[26];
-    	float d = ((0.074F-0.038F)*n)/((n-1)*this.indiceCoincidence(entree)-n*0.038F+0.074F);
-    	
-    	if (d <= 0.036F){
-    		return 8;
-    	}else if(d > 0.036F && d <= 0.036F){
-    		return 1257;
-    	}else if(d > 0.046F && d <= 0.056F){
-    		return 4;
-    	}else if(d > 0.056F && d <= 0.066F){
-    		return 368;
-    	}else if(d > 0.066F){
-    		return 9;
-    	}
-		return 0;
-    	
+    	//initialisation des tableaux
+    	String retour[] = new String[25];
+    	String clee[] = new String[25];
+    	for (int i = 0; i < retour.length ; i++) {
+			retour[i] = new String();
+			clee[i] = new String();
+		}
+    	//chaque case du tableau recoit comme si la longueur de la clee etait de longueur i+2
+    	for (int i = 0; i < retour.length; i++) {
+			clee[i] = this.trouverCle(entree, i+1);
+			retour[i] = this.deCoder(entree, clee[i]);
+		}
+    	//si une des case a pour indice de coincidence 0.778 +- 0.05 alors c'est le message decoder
+    	for (int i = 0; i < retour.length; i++) {
+    		System.out.println(this.indiceCoincidence(retour[i]));
+			if(this.indiceCoincidence(retour[i])>=0.072 && this.indiceCoincidence(retour[i])<=0.082)
+			{
+				
+				return retour[i];
+			}
+		}
+    	return "Non trouvé";
     }
     
-//    public String trouverClee(String entree, int longueur)
-//    {
-//    	String retour = "";
-//    	for (int i = 0; i < entree.length(); i++) {
-//			
-//		}
-//    }
+    
+    
+    
+    /*
+     * Fonction qui renvoie la clée en fonction du texte et de la longueur
+     * présumée de la clé
+     */
+    public String trouverCle(String entree,int longueur)
+    {
+    	String msg[] = new String[longueur];
+    	String retour = new String();
+    	/*
+    	 * Initialisation du tableau de messages
+    	 */
+    	for (int i = 0; i < longueur; i++) {
+			msg[i] = new String();
+		}
+    	/*
+    	 * On applique la methode césar a tout les tableau
+    	 */
+    	for (int i = 0; i < longueur; i++) {
+			for (int j = 0+i; j < entree.length()-longueur; j+=longueur) {
+				msg[i] = msg[i].concat(entree.valueOf(entree.charAt(j)));
+			}
+			retour = retour.concat(retour.valueOf(c.trouverClee(msg[i])));
+		}
+    	return retour;
+    }
 
 }
